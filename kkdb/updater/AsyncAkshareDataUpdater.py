a67a3fcf12bb78c63e5bb2f6383dd6f2e8fc5afb
simonwsy <wangshengyang2004@163.com>
2024-06-21 11:38:39 +0800

    update

--- kkdb/updater/AsyncAkshareDataUpdater.py
+++ kkdb/updater/AsyncAkshareDataUpdater.py
@@ -7,14 +7,20 @@ from retry import retry
 from typing import Iterable
 import os
 from loguru import logger
+
 # 清除环境变量中的代理设置
-os.environ.pop('http_proxy', None)
-os.environ.pop('https_proxy', None)
+os.environ.pop("http_proxy", None)
+os.environ.pop("https_proxy", None)
+
 
 class AsyncAkshareDataUpdater(AsyncBaseDataUpdater):
     def __init__(
-            self, db_name: str, bar_sizes: Iterable[str], client_str: str,
-            max_concurrent_requests: int = 3, session: ClientSession = None
+        self,
+        db_name: str,
+        bar_sizes: Iterable[str],
+        client_str: str,
+        max_concurrent_requests: int = 10,
+        session: ClientSession = None,
     ) -> None:
         super().__init__(db_name, bar_sizes, max_concurrent_requests, client_str)
         self.market_type = "cn_stock"
@@ -37,7 +43,7 @@ class AsyncAkshareDataUpdater(AsyncBaseDataUpdater):
                 "涨跌额": "change_amount",
                 "换手率": "turnover_rate",
             },
-            inplace=True
+            inplace=True,
         )
         df["timestamp"] = pd.to_datetime(df["timestamp"])
         df = df.loc[:, ~df.columns.duplicated()]
@@ -45,19 +51,28 @@ class AsyncAkshareDataUpdater(AsyncBaseDataUpdater):
 
     async def _get_stock_list(self) -> list:
         # Run akshare synchronously in an executor
-        stock_list_df = await asyncio.get_event_loop().run_in_executor(None, ak.stock_zh_a_spot_em)
+        stock_list_df = await asyncio.get_event_loop().run_in_executor(
+            None, func=ak.stock_zh_a_spot_em
+        )
         return stock_list_df["代码"].tolist()
 
     async def initialize_update(self):
-        stock_list = await  self._get_stock_list()
+        stock_list = await self._get_stock_list()
         self.stock_list = stock_list
 
     async def _get_existing_data_range(self, stock_code: str, collection_name: str):
         collection = self.db[collection_name]
-        earliest_record = await collection.find_one({'orderbook_id': stock_code}, sort=[("timestamp", 1)])
-        latest_record = await collection.find_one({'orderbook_id': stock_code}, sort=[("timestamp", -1)])
-        earliest = earliest_record['timestamp'] if earliest_record else None
-        latest = latest_record['timestamp'] if latest_record else None
+        earliest_record = await collection.find_one(
+            {"orderbook_id": stock_code}, sort=[("timestamp", 1)]
+        )
+        latest_record = await collection.find_one(
+            {"orderbook_id": stock_code}, sort=[("timestamp", -1)]
+        )
+        earliest = earliest_record["timestamp"] if earliest_record else None
+        latest = latest_record["timestamp"] if latest_record else None
+        logger.debug(
+            f"Existing data range for {stock_code} in {collection_name}: {earliest} - {latest}"
+        )
         return earliest, latest
 
     @retry(tries=3, delay=5)
@@ -66,22 +81,42 @@ class AsyncAkshareDataUpdater(AsyncBaseDataUpdater):
             await self.fetch_and_process_data(stock_code, freq)
 
     def download_data(self, stock_code, freq, start_date, end_date) -> pd.DataFrame:
+        logger.info(f"Downloading {stock_code} {freq} data...")
         if freq == "1m":
             return ak.stock_zh_a_minute(symbol=stock_code)
         elif freq == "1D":
-            return ak.stock_zh_a_hist(symbol=stock_code, period="daily", adjust="hfq", start_date=start_date, end_date=end_date)
+            return ak.stock_zh_a_hist(
+                symbol=stock_code,
+                period="daily",
+                adjust="hfq",
+                start_date=start_date,
+                end_date=end_date,
+            )
         elif freq == "1W":
-            return ak.stock_zh_a_hist(symbol=stock_code, period="weekly", adjust="hfq", start_date=start_date,
-                                      end_date=end_date)
+            return ak.stock_zh_a_hist(
+                symbol=stock_code,
+                period="weekly",
+                adjust="hfq",
+                start_date=start_date,
+                end_date=end_date,
+            )
         elif freq == "1M":
-            return ak.stock_zh_a_hist(symbol=stock_code, period="monthly", adjust="hfq", start_date=start_date,
-                                      end_date=end_date)
+            return ak.stock_zh_a_hist(
+                symbol=stock_code,
+                period="monthly",
+                adjust="hfq",
+                start_date=start_date,
+                end_date=end_date,
+            )
         else:
             raise ValueError(f"Invalid frequency: {freq}")
 
     async def fetch_and_process_data(self, stock_code, freq):
         collection_name = f"kline-{freq}"
-        earliest, latest = await self._get_existing_data_range(stock_code, collection_name)
+        earliest, latest = await self._get_existing_data_range(
+            stock_code, collection_name
+        )
+
         # Define the function to get data with time filtering
         def fetch_data():
             if earliest and latest:
@@ -96,6 +131,7 @@ class AsyncAkshareDataUpdater(AsyncBaseDataUpdater):
         if not stock_data_df.empty:
             processed_df = await self._process_df(stock_data_df)
             await self.insert_data(collection_name, processed_df)
+
     async def pool_download(self):
         tasks = []
         for stock_code in self.stock_list:
@@ -107,7 +143,7 @@ class AsyncAkshareDataUpdater(AsyncBaseDataUpdater):
     async def main(self):
         await self.drop_db(refresh=False)
         for bar_size in self.bar_sizes:
-            await self.create_timeseries_collection(f"kline-{bar_size}", 'timestamp')
+            await self.create_timeseries_collection(f"kline-{bar_size}", "timestamp")
         await self.start_session()
         logger.info("Starting data update...")
         await self.initialize_update()
@@ -118,5 +154,7 @@ class AsyncAkshareDataUpdater(AsyncBaseDataUpdater):
 
 if __name__ == "__main__":
     client_str = "mongodb://10.201.8.215:27017"  # Make sure to define this
-    updater = AsyncAkshareDataUpdater(db_name="cn_stock", bar_sizes=["1m", "1D", "1W","1M"], client_str=client_str)
+    updater = AsyncAkshareDataUpdater(
+        db_name="cn_stock", bar_sizes=["1m", "1D", "1W", "1M"], client_str=client_str
+    )
     asyncio.run(updater.main(refresh=False))
